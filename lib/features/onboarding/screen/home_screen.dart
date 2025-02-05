ce0b805cd7f1b4e5efe071edcdf7ae3e7315dfd8
Eslamadelelkhouly <rodyerak@gmail.com>
2025-02-05 23:04:43 +0200

    create custom card seller

--- lib/features/onboarding/screen/home_screen.dart
+++ lib/features/onboarding/screen/home_screen.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:nftmarketplace/core/resources/color_manager.dart';
 import 'package:nftmarketplace/core/resources/manage_style_text.dart';
 import 'package:nftmarketplace/core/resources/string_manager.dart';
-import 'package:nftmarketplace/features/onboarding/widgets/container_two_home.dart';
+import 'package:nftmarketplace/features/onboarding/widgets/custom_card_seller.dart';
 import 'package:nftmarketplace/features/onboarding/widgets/custom_list_view_container_one.dart';
 import 'package:nftmarketplace/features/onboarding/widgets/list_view_container_two.dart';
 
@@ -25,33 +25,46 @@ class HomeScreen extends StatelessWidget {
         child: Container(
           width: double.infinity,
           height: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              SizedBox(height: 15),
-              const CustomListViewContainerOne(),
-              SizedBox(height: 30),
-              Align(
-                alignment: Alignment.centerLeft,
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 15),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        textAlign: TextAlign.start,
-                        'Trending Collections',
-                        style: ManageStyleText.TitleList2,
-                      ),
-                      SizedBox(
-                        height: 15,
-                      ),
-                      const ListViewContainerTwo(),
-                    ],
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 15),
+                const CustomListViewContainerOne(),
+                SizedBox(height: 30),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: const EdgeInsets.only(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          textAlign: TextAlign.start,
+                          'Trending Collections',
+                          style: ManageStyleText.TitleList2,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        const ListViewContainerTwo(),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        Text(
+                          'Top Seller',
+                          style: ManageStyleText.TitleList2,
+                        ),
+                        SizedBox(
+                          height: 15,
+                        ),
+                        CustomCardSeller(),
+                      ],
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
---
+++ lib/features/onboarding/widgets/custom_card_seller.dart
@@ -0,0 +1,114 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+import 'package:nftmarketplace/core/resources/asset_image_manger.dart';
+import 'package:nftmarketplace/core/resources/color_manager.dart';
+import 'package:nftmarketplace/core/resources/manage_style_text.dart';
+
+class CustomCardSeller extends StatefulWidget {
+  CustomCardSeller({super.key});
+  Color coloricon = Colors.grey;
+  @override
+  State<CustomCardSeller> createState() => _CustomCardSellerState();
+}
+
+class _CustomCardSellerState extends State<CustomCardSeller> {
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      child: BackdropFilter(
+        filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+        child: Container(
+          padding: const EdgeInsets.only(top: 20),
+          width: 170,
+          height: 240,
+          color: ColorManager.white.withOpacity(0.5),
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(20),
+                width: 140,
+                height: 105,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage(AssetImageManger.Image1ContainerOne),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'data',
+                        style: ManageStyleText.textStyleDescription.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '##data',
+                        style: ManageStyleText.textStyleDescription,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 3),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Icon(Icons.hail),
+                    Text('data'),
+                    Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 30),
+                          child: IconButton(
+                            onPressed: () {
+                              setState(() {
+                                if (widget.coloricon == Colors.grey) {
+                                  widget.coloricon = Colors.red;
+                                } else {
+                                  widget.coloricon = Colors.grey;
+                                }
+                              });
+                            },
+                            icon: Icon(
+                              Icons.favorite,
+                              size: 17,
+                              fill: double.minPositive,
+                              color: widget.coloricon,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: Text(
+                            '200',
+                            style: ManageStyleText.textStyleDescription,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+      borderRadius: BorderRadius.circular(27),
+    );
+  }
+}
