7980e5def4a81a1ad305e16f324b65a05c96a001
Eslamadelelkhouly <rodyerak@gmail.com>
2025-02-13 14:22:04 +0200

    create row category

--- lib/features/onboarding/screen/stastics_screen.dart
+++ lib/features/onboarding/screen/stastics_screen.dart
@@ -43,50 +43,70 @@ class _StasticsScreenState extends State<StasticsScreen> {
               style: ManageStyleText.textStyleHeadingHome,
             ),
           ),
-          body: Column(
-            children: [
-              SizedBox(height: 10),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        isRankActive = true;
-                        isActivityActive = false;
-                      });
-                    },
-                    child: CartTitleStasticsScreen(
-                      icon: Icon(Icons.stacked_bar_chart),
-                      text: 'Ranking',
-                      active: isRankActive,
+          body: Container(
+            width: double.infinity,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                SizedBox(height: 10),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isRankActive = true;
+                          isActivityActive = false;
+                        });
+                      },
+                      child: CartTitleStasticsScreen(
+                        icon: Icon(Icons.stacked_bar_chart),
+                        text: 'Ranking',
+                        active: isRankActive,
+                      ),
                     ),
-                  ),
-                  GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        isActivityActive = true;
-                        isRankActive = false;
-                      });
-                    },
-                    child: ActivityTitleCard(
-                      icon: Icon(Icons.add_chart_rounded),
-                      text: 'Activity',
-                      active: isActivityActive,
+                    GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isActivityActive = true;
+                          isRankActive = false;
+                        });
+                      },
+                      child: ActivityTitleCard(
+                        icon: Icon(Icons.add_chart_rounded),
+                        text: 'Activity',
+                        active: isActivityActive,
+                      ),
                     ),
+                  ],
+                ),
+                Divider(thickness: 0.6, color: Colors.grey),
+                SizedBox(
+                  height: 27,
+                ),
+                Container(
+                  width: 330,
+                  child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CustomCategoryStasticScreen(
+                        text: 'All categories',
+                        iconstart: Icon(Icons.grid_view),
+                        iconend: Icon(Icons.expand_more),
+                      ),
+                      SizedBox(
+                        width: 25,
+                      ),
+                      CustomCategoryStasticScreen(
+                        text: 'All Chains',
+                        iconstart: Icon(Icons.insert_link),
+                        iconend: Icon(Icons.expand_more),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-              Divider(thickness: 0.6, color: Colors.grey),
-              SizedBox(
-                height: 27,
-              ),
-              CustomCategoryStasticScreen(
-                text: 'All categories',
-                iconstart: Icon(Icons.grid_view),
-                iconend: Icon(Icons.expand_more),
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
         Positioned(
